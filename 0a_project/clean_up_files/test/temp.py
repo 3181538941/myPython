aafa4315815aa530a610c0bb051bbfde3fe50200
leo <3181538941@qq.com>
2022-01-14 17:45:27 +0800

    解决命令行输入乱码问题, 使用""包裹文件路径, 防止路径中出现空格

--- 0a_project/clean_up_files/test/temp.py
+++ 0a_project/clean_up_files/test/temp.py
@@ -5,8 +5,13 @@ from pprint import pprint
 class ViewFile(object):
     def __init__(self, path, kwargs):
         self.allFile = {}
+        for typ in kwargs.keys():
+            self.allFile[typ] = []  # 按键初始化self.allFile
+        for p in kwargs.values():
+            if not os.path.isdir(p):  # 路径不是文件夹, 即不存在时 创建文件夹
+                os.makedirs(p)
         self.path = path
-        self.toMove = [] # 需要移动文件列表, [文件名, 所在目录, 绝对路径]
+        self.toMove = []  # 需要移动文件列表, [文件名, 所在目录, 绝对路径]
         self.clean1(path, kwargs)
 
     def viewTarget(self, dirPath):
@@ -47,19 +52,21 @@ class ViewFile(object):
                     return 1
             return 0
 
-
         for typ in kwargs.keys():  # 按文件类型循环
             for dirPath, dirNames, fileNames in os.walk(path):
                 for file in fileNames:  # 在dirPath文件夹内循环
                     if dirPath != kwargs[typ] and isInKey(file, typ):  # 要查找的文件类型不在选定文件夹内
-                        self.toMove.append([file, dirPath,os.path.join(dirPath,file)])
+                        self.toMove.append([file, typ, dirPath, os.path.join(dirPath, file)])
                         # self.toMove.append(file)
-                        if typ not in self.allFile.keys():
-                            self.allFile[typ] = []
                         self.allFile[typ].append(file)
+
                     # pprint(dirPath)
                     # pprint(dirNames)
                     # pprint(fileNames)
+        first = lambda li: [_[0] for _ in li]
+        self.toMoveFlie = first(self.toMove)
+        for item in self.toMove:
+            os.popen(rf'move "{item[3]}" "{kwargs[item[1]]}"')
 
 
 # path = r'F:\03_Important\Python\0a_project\clear_up_files'
@@ -68,10 +75,11 @@ path = r'E:\Download\云盘缓存'
 # dic = {'exe': r'E:\Download\exes',
 #        'zip': r'E:\Download\Compressed',
 #        'rar': r'E:\Download\Compressed'}
-dic = {'exe'       : r'E:\Download\云盘缓存\exes',
+dic = {'exe'       : r'E:\Download\云盘缓存\Executable',
        'zip rar 7z': r'E:\Download\云盘缓存\Compressed'}
 
 list = ViewFile(path, dic)
 pprint(list.allFile)
 print('toMove')
 pprint(list.toMove)
+print(list.toMoveFlie)
